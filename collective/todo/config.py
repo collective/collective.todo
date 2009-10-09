0545b84ccdcc66957a6f0a135c93b8f013d7fee3
aclark <aclark@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-10-09 19:27:34 +0000

    Fix perms

--- collective/todo/config.py
+++ collective/todo/config.py
@@ -6,5 +6,4 @@ PROJECTNAME = 'collective.todo'
 ADD_PERMISSIONS = {
     # -*- extra stuff goes here -*-
     'Todo item': 'collective.todo: Add Todo item',
-    'Todo folder': 'collective.todo: Add Todo folder',
 }
